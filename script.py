92849b0d87e3be2f673734d089616c2f53b09274
JINN-OF-TG <84127769+JINN-OF-TG@users.noreply.github.com>
2021-05-26 14:36:53 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,69 +1,23 @@
 class script(object):
 
 
-    START_MSG = """ <b>Hi {}
-
-You can call this as an Auto Filter Bot if you like :D
-
-This is Version 2 of Auto Filter Bot
-
-Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
-
-For more click <i>help</i></b>"""
+    START_MSG = """ഹായ് {}
+ഞാൻ FILM CORNER ഗ്രൂപ്പിൽ വർക്ക് ചെയ്യുന്ന ഒരു പാവം ഓട്ടോഫിൽട്ടർ ബോട്ടാണ്. എന്ന് കരുതി എന്നെ മറ്റ് ഗ്രൂപ്പിൽ ആഡ് ആക്കാൻ പറ്റില്ല.
+പിന്നെ എന്തായാലും ഇവിടെ വരെ വന്നതല്ലേ ഞങ്ങളുടെ മൂവീ ഗ്രൂപ്പിലൂടേയും ഒന്ന് ജോയിൻ ആയേര്😎😎.
+"""
 
 
-    HELP_MSG = """<b>How to use the bot??</b>
-
-<i>
-* Add bot to your group with admin rights.
-
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
+    HELP_MSG = """
+    എനിക്ക് നിൻ്റെ സഹായം ഒന്നും വേണ്ട അവൻ സഹായം ചോയിച്ച് വന്നേക്കണ്🐔🐔.. 
+"""
 
 
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
+    ABOUT_MSG = """
 
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
+🛡️<b> മൂവീഗ്രൂപ്പ് :</b> <a href='https://t.me/filmcornermm'>CLICK TO JOIN</a> 
+🛡️<b> മൂവീചാനൽ :</b> <a href='https://t.me/publicchannalin'>CLICK TO JOIN</a> 
 
 
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @TroJanzHEX</b>"""
-
-
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
-    
-⭕️<b>Creater :</b> @TroJanzHEX
-
-⭕️<b>Language :</b> <code>Python3</code>
-
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+  ❣️JOIN @NAZRIYANAZEEM_BETA❣️
 
 """
